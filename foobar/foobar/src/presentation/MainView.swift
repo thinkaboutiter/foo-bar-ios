944d1a8335f9e0dc7033105c61a53485c8558e94
Thinka Boutiter <thinkaboutiter@gmail.com>
2025-06-24 07:21:24 +0300

    minor refactor, add mock data

--- foobar/foobar/src/presentation/MainView.swift
+++ foobar/foobar/src/presentation/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  foobar
 //
 //  Created by boyan.yankov on 2025-06-24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MainView()
 }
