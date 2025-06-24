561ea6cbf1fec914f164668e995f22df31dcd2db
Thinka Boutiter <thinkaboutiter@gmail.com>
2025-06-24 07:11:19 +0300

    add initial project

---
+++ foobar/foobar/foobarApp.swift
@@ -0,0 +1,17 @@
+//
+//  foobarApp.swift
+//  foobar
+//
+//  Created by boyan.yankov on 2025-06-24.
+//
+
+import SwiftUI
+
+@main
+struct foobarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
